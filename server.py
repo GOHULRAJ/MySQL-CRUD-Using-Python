ea4fb9097144430b6226e423ca12ec49d25b58d5
GOHULRAJ <gohulrajs71@gmail.com>
2024-03-31 00:05:00 +0530

    Database connected

--- server.py
+++ server.py
@@ -1,1 +1,16 @@
-print("Hello world")
+import mysql.connector
+
+mydb=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root",
+    database="testdatabase",
+    port="3306"
+)
+
+mycursor=mydb.cursor()
+
+mycursor.execute("SHOW TABLES")
+
+for db in mycursor:
+    print(db)
